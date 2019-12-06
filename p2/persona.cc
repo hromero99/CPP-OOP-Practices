87ba3d1e9b8fbf93593490178931e16cb2f7cb90
Héctor <hromerolopez@protonmail.com>
2019-12-06 15:46:30 +0100

    Added makefile and test for cruipier and persona

--- p2/persona.cc
+++ p2/persona.cc
@@ -1,8 +1,8 @@
 #include "persona.h"
 
-Persona::Persona(const std::string & dni, const std::string & nombre, const std::string & apellidos , const std::string & direccion, \
-    const std::string &localidad, const std::string & provincia,const std::string & pais, int edad){
-        setDni(dni);
+Persona::Persona(const std::string & dni, const std::string & nombre, const std::string & apellidos , int edad, const std::string & direccion, \
+    const std::string &localidad, const std::string & provincia,const std::string & pais ){
+        setDNI(dni);
         setNombre(nombre);
         setApellidos(apellidos);
         setDireccion(direccion);
